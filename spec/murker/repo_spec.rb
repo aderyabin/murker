b4fb222072ed3e03e5c97a934d5dba04c7633712
Alexey Vasilyev <bestspajic@gmail.com>
2018-01-01 22:12:23 +0300

    Update test

--- spec/murker/repo_spec.rb
+++ spec/murker/repo_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe Murker::Repo do
 
     context 'when interaction is GET v1/martians/:id' do
       let(:params) { { verb: 'GET', endpoint_path: 'v1/martians/:id' } }
-      expected = 'murker/v1/martians/__id/GET.txt'
+      expected = 'spec/murker/v1/martians/__id/GET.txt'
 
       it { expect(repo.path_for(interaction)).to eq expected }
     end
 
     context 'when interaction is GET v1/martians.json' do
       let(:params) { { verb: 'GET', endpoint_path: 'v1/martians' } }
-      expected = 'murker/v1/martians/GET.txt'
+      expected = 'spec/murker/v1/martians/GET.txt'
 
       it { expect(repo.path_for(interaction)).to eq expected }
     end
