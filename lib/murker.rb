77d87e504432f576010602811065cc68bd2fbd21
Alexey Vasilyev <bestspajic@gmail.com>
2018-01-02 20:39:24 +0300

    Implement first draft of OpenApiGenerator

--- lib/murker.rb
+++ lib/murker.rb
@@ -1,6 +1,7 @@
 require 'murker/version'
 require 'murker/spy'
 require 'murker/interaction'
+require 'murker/open_api_generator'
 
 module Murker
   def self.capture(&block)
---
+++ spec/murker/open_api_generator_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+RSpec.describe Murker::OpenApiGenerator do
+  describe '#call' do
+    let(:generator) { described_class.new(interaction) }
+    let(:interaction) { Murker::Interaction.new(params) }
+
+    context 'when interaction is GET /martians.json' do
+      let(:params) {
+        {
+          verb: 'GET',
+          endpoint_path: '/martians',
+          status: 200,
+          body: [{"name"=>"spajic", "age"=>30, "ololo"=>"OLOLO"}]
+        }
+      }
+      expected = <<~HEREDOC
+        ---
+        openapi: 3.0.0
+        paths:
+          "/martians":
+            get:
+              responses:
+                "'200'":
+                  content:
+                    application/json:
+                      schema:
+                        type: array
+                        minItems: 1
+                        uniqueItems: true
+                        items:
+                          type: object
+                          required:
+                          - name
+                          - age
+                          - ololo
+                          properties:
+                            name:
+                              type: string
+                            age:
+                              type: integer
+                            ololo:
+                              type: string
+      HEREDOC
+
+      it { expect(generator.call).to eq expected }
+    end
+
+    # context 'when interaction is GET v1/martians/:id' do
+    #   let(:params) { { verb: 'GET', endpoint_path: 'v1/martians/:id' } }
+    #   expected = 'spec/murker/v1/martians/__id/GET.txt'
+
+    #   it { expect(repo.path_for(interaction)).to eq expected }
+    # end
+  end
+end
