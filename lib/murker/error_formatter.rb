bcc90060f8dfad684a4f664d86d075ff9f8ea4a9
Alexey Vasilyev <bestspajic@gmail.com>
2018-01-09 02:02:04 +0300

    Disable color diff yet

--- lib/murker/error_formatter.rb
+++ lib/murker/error_formatter.rb
@@ -8,11 +8,7 @@ module Murker
     def self.call(new_schema, stored_schema)
       new_yaml = YAML.dump new_schema
       stored_yaml = YAML.dump stored_schema
-      Diffy::Diff.new(stored_yaml, new_yaml).to_s(diffy_format)
-    end
-
-    def self.diffy_format
-      ENV['RAILS_ENV'] == 'test' ? :color : :text
+      Diffy::Diff.new(stored_yaml, new_yaml).to_s
     end
   end
 end
