21cac0a370d4009f65abb8778774f1a8f7dab200
Alexey Vasilyev <bestspajic@gmail.com>
2018-01-08 19:53:01 +0300

    Add some space

--- test_apps/rails_51/app/controllers/v1/martians_controller.rb
+++ test_apps/rails_51/app/controllers/v1/martians_controller.rb
@@ -2,6 +2,7 @@ module V1
   class MartiansController < ActionController::API
     def index
       return render plain: '' if params[:respond_with_broken_json]
+
       render json: Martian.all
     end
 
